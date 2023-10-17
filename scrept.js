ae9b117f8e71d4e82411e554754a24fde328d213
miguel da silva <miguelpositivo02@gmail.com>
2023-10-16 21:52:34 -0300

    Commit

---
+++ scrept.js
@@ -0,0 +1,5 @@
+let nome,sobrenome;
+nome = prompt("digite seu nome");
+sobrenome=prompt("digite seu sobrenome");
+alert(`Nome completo:  ${nome} ${sobrenome}`);
+alert(`Nome de catálogo: ${sobrenome.toUpperCase()},${nome}`);
